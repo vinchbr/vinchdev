c9b859c8edf47701165c446c95167b7fe8fe657a
Vicenzo Naves <vicenzo@gmail.com>
2023-02-28 11:31:48 -0800

    Setuo GH Actions and GH Page

--- src/features/catCard/CatCard.tsx
+++ src/features/catCard/CatCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { CategorizedCats, CatImage } from "../../types";
-import { Card, Image } from "semantic-ui-react";
+import { Card, Image, Message } from "semantic-ui-react";
 import { useAppSelector } from "../../app/hooks";
 import { breedMenuActiveState } from "../breedMenu/breedMenuSlice";
 
@@ -16,12 +16,21 @@ export const CatCard: React.FC<Props> = ({ categorizedCats }) => {
       : categorizedCats[catCategory];
   return (
     <Card.Group doubling itemsPerRow={3} stackable>
-      {catImages?.map((catImage) => (
-        <Card key={catImage.id}>
-          <Image src={catImage.url} />
-          {/*<Card.Content></Card.Content>*/}
-        </Card>
-      ))}
+      {catImages ? (
+        catImages.map((catImage) => (
+          <Card key={catImage.id}>
+            <Image src={catImage.url} />
+            {/*<Card.Content></Card.Content>*/}
+          </Card>
+        ))
+      ) : (
+        <Message>
+          <Message.Header>Failed to load images</Message.Header>
+          <p>
+            Unfortunately the cats you are looking for are in another castle...
+          </p>
+        </Message>
+      )}
     </Card.Group>
   );
 };
--- src/services/cats.ts
+++ src/services/cats.ts
@@ -1,25 +1,41 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { Breed, ImageResponse } from "../types";
+import { Breed, CategorizedCats, CatImage, ImageResponse } from "../types";
 
 export const catApi = createApi({
-    reducerPath: 'catApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://api.thecatapi.com/v1',
-        prepareHeaders: (headers: Headers) => {
-            headers.set('x-api-key', 'live_JsefUSEXScyhWOVVH7WxRrfiLNbCsSdX4BTx5qQh9D54GCOfjJBGarnk8yqkKcoW')
-        },
+  reducerPath: "catApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.thecatapi.com/v1",
+    prepareHeaders: (headers: Headers) => {
+      headers.set(
+        "x-api-key",
+        "live_JsefUSEXScyhWOVVH7WxRrfiLNbCsSdX4BTx5qQh9D54GCOfjJBGarnk8yqkKcoW"
+      );
+    },
+  }),
+  tagTypes: ["CatImages", "CatBreedImages", "BreedsList"],
+  endpoints: (builder) => ({
+    getImages: builder.query<CategorizedCats, void>({
+      query: () => "/images/search?limit=20",
+      providesTags: ["CatImages"],
+      transformResponse: (response: ImageResponse) =>
+        response.reduce((acc: CategorizedCats, curr: CatImage) => {
+          const breedName: string =
+            curr.breeds.length > 0 ? curr.breeds[0].name : "unknown";
+
+          if (!acc[breedName]) {
+            acc[breedName] = [];
+          }
+
+          acc[breedName].push(curr);
+
+          return acc;
+        }, {}),
     }),
-    tagTypes: ['CatImages', 'CatBreedImages', 'BreedsList'],
-    endpoints: (builder) => ({
-        getImages: builder.query<ImageResponse, void>({
-            query: () => '/images/search?limit=20',
-            providesTags: ['CatImages']
-        }),
-        filterByBreed: builder.query<Breed, string>({
-            query: (breedId) => `/images/search?breed_ids=${breedId}`,
-            providesTags: (result, breedId) => [{ type: 'CatBreedImages', breedId}]
-        }),
-    })
-})
+    filterByBreed: builder.query<Breed, string>({
+      query: (breedId) => `/images/search?breed_ids=${breedId}`,
+      providesTags: (result, breedId) => [{ type: "CatBreedImages", breedId }],
+    }),
+  }),
+});
 
 export const { useGetImagesQuery, useFilterByBreedQuery } = catApi;
